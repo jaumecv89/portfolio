248b970f7db25b724de02f2c4e46ed100e2dfb07
Jaume <jaume.cvila@gmail.com>
2020-10-01 01:40:04 +0200

    Added responsive navbar, logo and visual changes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,13 @@
 $(function() {
+    var btn = $('.nav-btn');
+
     $('a[href*=\\#]').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
+
+    btn.on('click', function() {
+        $(this).toggleClass('active');
+        $(this).toggleClass('not-active');
+      });
 });
