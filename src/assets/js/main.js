77589498fafbb9bf987aefec693b7a49a681c5c3
Jaume <jaume.cvila@gmail.com>
2020-10-02 16:16:10 +0200

    Fixed some problems with the responsive menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,7 @@ $(function() {
     var navigation = $('nav');
     var responsiveMenu = $('.responsive-menu');
     var navLinks = $('.nav-links');
+    var navLinksButton = $('.nav-links a');
 
     $('a[href*=\\#]').on('click', function(e) {
         e.preventDefault();
@@ -12,6 +13,13 @@ $(function() {
     btn.on('click', function() {
         $(this).toggleClass('active');
         $(this).toggleClass('not-active');
+        $(responsiveMenu).toggleClass('show');
+        $(navLinks).toggleClass('show');
+    });
+
+    navLinksButton.on('click', function() {
+        $(btn).toggleClass('active');
+        $(btn).toggleClass('not-active');
         $(responsiveMenu).toggleClass('show');
         $(navLinks).toggleClass('show');
     });
