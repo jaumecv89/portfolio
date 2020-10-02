cb0c3801498d74a98d9544cc699a4bb42d5866ab
Jaume <jaume.cvila@gmail.com>
2020-10-02 17:10:36 +0200

    Added the active links logic

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,19 @@
 $(function() {
     var btn = $('.nav-btn');
-    var navigation = $('nav');
     var responsiveMenu = $('.responsive-menu');
     var navLinks = $('.nav-links');
+    var navLinksA = $('.nav-links a');
     var navLinksButton = $('.nav-links a');
+
+    function setActive(i) {
+        navLinksA.each(function () {
+            if($(this).attr('href') == '#' + i) {
+                $(this).addClass("active");
+            } else {
+                $(this).removeClass("active");
+            }
+        });
+    }
 
     $('a[href*=\\#]').on('click', function(e) {
         e.preventDefault();
@@ -22,5 +32,13 @@ $(function() {
         $(btn).toggleClass('not-active');
         $(responsiveMenu).toggleClass('show');
         $(navLinks).toggleClass('show');
+    });
+
+    $(document).scroll(function () {
+        $('section').each(function () {
+            if($(this).position().top <= $(document).scrollTop() && ($(this).position().top + $(this).outerHeight()) > $(document).scrollTop()) {
+                setActive($(this).attr('id'));
+            }
+        });
     });
 });
