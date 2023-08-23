dc7fd7413957c85eb94eb610e8bdcc237a01d6e9
Jaume Campderros <jaume.cvila@gmail.com>
2023-08-23 19:13:36 +0200

    added improvements

---
+++ src/constants/TechStack.tsx
@@ -0,0 +1,48 @@
+import graphql from "../assets/Icons/icon-graphql.svg"
+import nextjs from "../assets/Icons/icon-nextjs.svg"
+import react from "../assets/Icons/icon-react.svg"
+import sass from "../assets/Icons/icon-sass.svg"
+import tailwind from "../assets/Icons/icon-tailwind.svg"
+import typescript from "../assets/Icons/icon-typescript.svg"
+import vite from "../assets/Icons/icon-vite.svg"
+import vitest from "../assets/Icons/icon-vitest.svg"
+import wordpress from "../assets/Icons/icon-wordpress.svg"
+
+export const TechStack = {
+    react: {
+        name: "React",
+        icon: react,
+    },
+    typescript: {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    tailwindcss: {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    vite: {
+        name: "Vite",
+        icon: vite,
+    },
+    vitest: {
+        name: "Vitest",
+        icon: vitest,
+    },
+    graphql: {
+        name: "GraphQL",
+        icon: graphql,
+    },
+    sass: {
+        name: "Sass",
+        icon: sass,
+    },
+    wordpress: {
+        name: "WordPress",
+        icon: wordpress,
+    },
+    nextjs: {
+        name: "NextJS",
+        icon: nextjs,
+    },
+}
